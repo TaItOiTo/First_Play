4c09c271d2a16eb32d9465fda5476e0b3509b5e4
TaItOiTo <taito.ito.mirai@gmail.com>
2017-05-26 13:57:38 +0900

    フィルターの修正

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,7 @@
+CREATE TABLE  if not EXISTS play_scala_intro.people(
+  id INT not NULL  PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(255) not NULL ,
+  age INT not NULL
+);
+
+drop TABLE  if EXISTS  popple;
